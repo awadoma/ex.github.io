e01e28a4f72f188c6285fb064a3188e309916993
awadoma <32640370+awadoma@users.noreply.github.com>
2018-05-03 11:15:16 +0200

    Add files via upload

---
+++ src/Convert.java
@@ -0,0 +1,15 @@
+
+public class Convert {
+
+	public static void main(String[] args) {
+		/*
+		int Numb = 2016;
+		String strnumb = String.valueOf(Numb);
+		System.out.println(strnumb);
+		*/
+		String strName = "2016";
+		int numb = Integer.parseInt(strName);
+		System.out.println(strName);
+	}
+
+}
